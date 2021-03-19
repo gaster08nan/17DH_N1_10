f370a3831106cd11d14d630dd40ee5848da732ae
Thang <huynhthang1108@gmail.com>
2021-03-19 15:51:54 +0700

    testtttt

--- 17DT_N10_N10/17DT_N10_N10/Program.cs
+++ 17DT_N10_N10/17DT_N10_N10/Program.cs
@@ -7,6 +7,7 @@ namespace _17DT_N10_N10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hellooooooooooooooooo World!");
         }
     }
 }
