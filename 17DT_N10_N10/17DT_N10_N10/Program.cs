bf2b15130a1b94e918445e1fbded8141645c24db
Thang <huynhthang1108@gmail.com>
2021-03-19 14:36:23 +0700

    ass

---
+++ 17DT_N10_N10/17DT_N10_N10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17DT_N10_N10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
